a4713b5072371bea974c1c1ba1f3a7f36287c439
Bruno Jacob <brunojacob_89@hotmail.com>
2022-10-12 19:07:29 -0300

    add put, del

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -3,11 +3,17 @@ package curso.api.rest.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,6 +43,33 @@ public class IndexController {
 		return new ResponseEntity<List<Usuario>> (list, HttpStatus.OK);
 	}
 	
+	@PostMapping(value = "/")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+		
+	}
+	
+	@PutMapping(value = "/")
+	@Transactional
+	public ResponseEntity<Usuario> atualizar( @RequestBody Usuario usuario) {
+		
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+		
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Usuario> deletar (@PathVariable("id") Long id){
+		
+		usuarioRepository.deleteById(id);
+		
+		return ResponseEntity.ok().build();
+	}
 	
 	
 //	/*Serviço RESTful*/
